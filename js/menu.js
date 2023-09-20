1929f9308c4a4d67e0795b5b63398dddc8f94e3c
Kacper Kwinta <kaspersky.kwinta@gmail.com>
2023-09-20 20:42:18 +0200

    mobile menu 1/2 done

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log(123);
+const menuHamburger = document.querySelector(".menu-hamburger");
+const menuClose = document.querySelector(".menu-close");
+
+function toggleIconMenu() {
+	menuClose.classList.toggle("hidden");
+	menuHamburger.classList.toggle("hidden");
+}
+
+menuHamburger.addEventListener("click", toggleIconMenu);
+menuClose.addEventListener("click", toggleIconMenu);
