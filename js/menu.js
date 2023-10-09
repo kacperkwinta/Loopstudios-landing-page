dab2cdbe119c7171b16ae169fbb04a2bccdf3028
Kacper Kwinta <kaspersky.kwinta@gmail.com>
2023-10-09 21:08:20 +0200

    mobile menu works

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,12 @@
 
 const menuHamburger = document.querySelector(".menu-hamburger");
 const menuClose = document.querySelector(".menu-close");
+const nav = document.querySelector("nav");
 
 function toggleIconMenu() {
 	menuClose.classList.toggle("hidden");
 	menuHamburger.classList.toggle("hidden");
+	nav.classList.toggle("mobile-nav");
 }
 
 menuHamburger.addEventListener("click", toggleIconMenu);
